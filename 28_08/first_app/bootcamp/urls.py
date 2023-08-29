45ca6e35798aefc88a9286b488ff7e53943185c7
Irving Suarez <irving.dev@icloud.com>
2023-08-28 18:50:07 -0600

    [28_ago]: Adding koder type template

--- 28_08/first_app/bootcamp/urls.py
+++ 28_08/first_app/bootcamp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import get_koder, list_koders
+from .views import get_koder, list_koders, get_koder_type
 
 urlpatterns = [
     path("get_koder/<int:id>", get_koder),
     path("list_koders", list_koders),
+    path("user/<str:name>/<str:type>", get_koder_type),
 ]
